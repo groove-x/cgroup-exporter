c0ede45844089f9ae12865b68b85a68b94ff3e7e
Ato Araki <ato.araki@groove-x.com>
2018-06-20 15:46:22 +0900

    add cgroup-exporter

---
+++ Makefile
@@ -0,0 +1,32 @@
+PACKAGE_NAME:=$(shell cat deb.json | jq -r '.name')
+VERSION:=$(shell cat deb.json | jq -r '.version')
+ARCH?=amd64
+
+test:
+	go-bin-deb test
+
+deb:
+	ARCH=amd64 make ${PACKAGE_NAME}_amd64.deb
+	ARCH=arm64 make ${PACKAGE_NAME}_arm64.deb
+
+${PACKAGE_NAME}_${ARCH}.deb: ${PACKAGE_NAME}_${VERSION}_${ARCH}.deb
+${PACKAGE_NAME}_${VERSION}_${ARCH}.deb: deb.json systemd/* $(APP_RESOURCES) build/${ARCH}/cgroup-exporter
+	rm -rf pkg-build
+	go-bin-deb generate --arch ${ARCH}
+
+clean:
+	rm -rf build
+	rm -rf pkg-build
+	rm -f ${PACKAGE_NAME}_*.deb
+
+install:
+	sudo dpkg -i ${PACKAGE_NAME}_${VERSION}_${ARCH}.deb
+
+uninstall:
+	sudo dpkg -r ${PACKAGE_NAME}
+
+build/${ARCH}/cgroup-exporter: deb.json
+	go get -d -t .
+	@rm -rf build/${ARCH} && mkdir -p build/${ARCH}
+	GOARCH=${ARCH} go build -o $@ -ldflags "-X main.version=${VERSION} -X main.git=${GIT_HASH}" .
+
---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/containerd/cgroups"
+)
+
+var (
+	versionFlag = flag.Bool("version", false, "version")
+	version     string
+	git         string
+)
+
+func main() {
+	flag.Parse()
+	if *versionFlag {
+		fmt.Printf("version %s, git %s\n", version, git)
+		return
+	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
+
+	system, err := cgroups.Load(subsystem, cgroups.StaticPath("/system.slice"))
+	if err != nil {
+		log.Fatalf("cgroups load: %s", err)
+	}
+
+	http.HandleFunc("/metrics", exportMetrics(system))
+
+	server := &http.Server{
+		Addr: "0.0.0.0:48900",
+	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("http server ListenAndServe: %v", err)
+		}
+	}()
+
+	<-sig
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("http server shutdown: %s", err)
+	}
+}
+
+var (
+	cpuUserTotalHeader = `# HELP container_cpu_user_seconds_total Cumulative user cpu time consumed in seconds.
+# TYPE container_cpu_user_seconds_total counter
+`
+	cpuUserTotalFormat = `container_cpu_user_seconds_total{id="%s"} %.2f
+`
+
+	memoryUsageHeader = `# HELP container_memory_usage_bytes Current memory usage in bytes.
+# TYPE container_memory_usage_bytes gauge
+`
+	memoryUsageFormat = `container_memory_usage_bytes{id="%s"} %d
+`
+)
+
+func subsystem() ([]cgroups.Subsystem, error) {
+	root := "/sys/fs/cgroup"
+	s := []cgroups.Subsystem{
+		cgroups.NewDevices(root),
+		cgroups.NewCpuacct(root),
+		cgroups.NewMemory(root),
+	}
+	return s, nil
+}
+
+func exportMetrics(system cgroups.Cgroup) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		processes, err := system.Processes(cgroups.Devices, true)
+		if err != nil {
+			msg := fmt.Sprintf("cgroups load: %s", err)
+			http.Error(w, msg, http.StatusInternalServerError)
+		}
+
+		groups := make(map[string]*cgroups.Metrics, len(processes))
+		for _, p := range processes {
+			name := strings.TrimPrefix(p.Path, "/sys/fs/cgroup/devices")
+			name = strings.TrimSuffix(name, "/")
+			if _, ok := groups[name]; ok {
+				continue
+			}
+
+			control, err := cgroups.Load(subsystem, func(subsystem cgroups.Name) (string, error) {
+				return name, nil
+			})
+			if err != nil {
+				log.Printf("cgroups load: %s", err)
+				continue
+			}
+			stats, err := control.Stat(cgroups.IgnoreNotExist)
+			if err != nil {
+				log.Printf("control stat: %s", err)
+				continue
+			}
+			groups[name] = stats
+		}
+
+		fmt.Fprint(w, cpuUserTotalHeader)
+		for name, stats := range groups {
+			fmt.Fprintf(w, cpuUserTotalFormat, name, float64(stats.CPU.Usage.User)/1000000000.0)
+		}
+		fmt.Fprint(w, memoryUsageHeader)
+		for name, stats := range groups {
+			fmt.Fprintf(w, memoryUsageFormat, name, stats.Memory.Usage.Usage)
+		}
+		return
+	}
+}
